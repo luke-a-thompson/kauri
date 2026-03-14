d4326c0dbe7e1124db4bd0004486edf079f3485f
Luke Thompson <luke.thompson@sydney.edu.au>
2026-03-14 20:11:08 +1100

    Feat: Add williamson recursion dataclass

--- kauri/methods/williamson.py
+++ kauri/methods/williamson.py
@@ -16,30 +16,46 @@ from kauri.rk_builder.williamson import (
     williamson_tableau_expressions,
 )
 
+@dataclass(frozen=True)
+class WilliamsonRecursion:
+    A: list[list[sympy.core.basic.Basic]]
+    B: list[sympy.core.basic.Basic]
+
+    def __post_init__(self) -> None:
+        stages = len(self.B)
+        if stages == 0:
+            raise ValueError("Parameter 'b' must be a non-empty vector")
+        if len(self.A) != stages or any(len(row) != stages for row in self.A):
+            raise ValueError("Parameter 'a' must be a square stages x stages matrix and b a vector of length stages")
+
+    @cached_property
+    def stages(self) -> int:
+        return len(self.B)
+
 
 @dataclass(frozen=True)
 class WilliamsonRK:
-    stages: int
-    A: list[sympy.core.basic.Basic]
-    B: list[sympy.core.basic.Basic]
+    recursion: WilliamsonRecursion
     name: str
 
     def __post_init__(self) -> None:
-        if len(self.A) != self.stages or len(self.B) != self.stages:
-            raise ValueError("Williamson A and B must both have length equal to stages")
-        if sympy.simplify(self.A[0]) != 0:
+        if any(sympy.simplify(x) != 0 for x in self.recursion.A[0]):
             raise ValueError("Williamson methods require A0 = 0")
+
+    @cached_property
+    def stages(self) -> int:
+        return self.recursion.stages
 
     @cached_property
     def tableau(self) -> ButcherTableau:
         a_expr, b_expr = williamson_tableau_expressions(
-            stages=self.stages,
-            A_symbols=self.A,
-            B_symbols=self.B,
+            stages=self.recursion.stages,
+            A_symbols=[sympy.Integer(0)] + [self.recursion.A[i][i - 1] for i in range(1, self.recursion.stages)],
+            B_symbols=self.recursion.B,
         )
         a = [
-            [sympy.simplify(a_expr[i_idx][j_idx]) for j_idx in range(self.stages)]
-            for i_idx in range(self.stages)
+            [sympy.simplify(a_expr[i_idx][j_idx]) for j_idx in range(self.recursion.stages)]
+            for i_idx in range(self.recursion.stages)
         ]
         b = [sympy.simplify(value) for value in b_expr]
         return ButcherTableau(a=a, b=b)
@@ -49,9 +65,9 @@ class WilliamsonRK:
             base=self,
             name=f"{self.name}_cf",
             stage_nodes=[sympy.simplify(value) for value in self.tableau.c],
-            storage_a=[sympy.simplify(value) for value in self.A],
-            exp_coeffs=[sympy.simplify(value) for value in self.B],
-            exponentials_per_update=self.stages,
+            storage_a=[sympy.simplify(self.recursion.A[i][i - 1] if i > 0 else sympy.Integer(0)) for i in range(self.recursion.stages)],
+            exp_coeffs=[sympy.simplify(value) for value in self.recursion.B],
+            exponentials_per_update=self.recursion.stages,
         )
 
 
@@ -146,5 +162,13 @@ def rk_to_williamson_2n(rk: RK) -> WilliamsonRK:
         )
 
     verify_williamson_relations(a=a, b=b, A_params=A_params, B=B)
+    recursion_a: list[list[sympy.core.basic.Basic]] = [
+        [sympy.Integer(0)] * stages for _ in range(stages)
+    ]
+    for i in range(1, stages):
+        recursion_a[i][i - 1] = A_params[i]
     method_name: str = rk.name if rk.name is not None else "unnamed_rk"
-    return WilliamsonRK(stages=stages, A=A_params, B=B, name=f"{method_name}_williamson2n")
+    return WilliamsonRK(
+        recursion=WilliamsonRecursion(A=recursion_a, B=B),
+        name=f"{method_name}_williamson2n",
+    )
